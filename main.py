03b6689575d1ba71b034eae241dc0e9aebbaba74
Kevin Dahlhoff <kdahlhoff3@gmail.com>
2024-09-12 13:38:28 +0200

    main logic bare bone fertiggestellt

--- main.py
+++ main.py
@@ -3,20 +3,50 @@ from datetime import datetime
 from settings.Settings import Settings
 from sql import Sql
 import os
+import uuid
+from random import randint
 
+sql = Sql.SQL(Settings.DatabasePath)
+# handling database setup (db creation, table creation)
 if not os.path.exists(Settings.DatabasePath):
-    sqlSetup = Sql.SqlSetup(Settings.DatabaseName)
-    sqlSetup.createDatabaseFromFile()
+    sql.createDatabaseFromFile()
 
+# creating SQL object
+conn = sql.open_conn()
+
+# game variables
+randomGameInt = randint(0, 101)
+numberOfAttempts = 0
+
+# create flask app
 app = Flask(__name__, static_folder='static')
+
+# generating unique game id
+gameId = str(uuid.uuid4())
 
 @app.route('/')
 def hello_world():
+    print(f'gameId: {gameId}')
+    print(randomGameInt)
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit_attempt():
-    # implement code
+    global numberOfAttempts  # Declare as global
+    global randomGameInt  # Declare as global to use in comparison
+
+    player_name = 'Testplayer' #fetch playername
+    number = request.form.get('number')
+    print(number)
+    # get player id from player_name?
+    temp_userID = str(uuid.uuid4())
+
+    numberOfAttempts += 1
+
+    if int(number) == int(randomGameInt):
+        sql.create_game(conn, gameId, temp_userID, numberOfAttempts)
+        print("you have won")
+        return render_template('won.html')
 
     return redirect(url_for('hello_world'))
 
--- sql/Sql.py
+++ sql/Sql.py
@@ -5,33 +5,22 @@ class SQL:
     def __init__(self, db):
         self.db = db
         self.conn = sqlite3.Connection
+        self.cursor = sqlite3.Cursor
 
     def open_conn(self):
         try:
-            self.conn = sqlite3.connect(self.db)
+            self.conn = sqlite3.connect(self.db, check_same_thread=False)
             self.cursor = self.conn.cursor()
             return self.conn
         except:
             return "SQL.open_conn: Error while opening database connection"
-        
-    def create(self):
-        pass
-
-    def read(self):
-        pass
-
-    def update(self):
-        pass
-
-    def delete(self):
-        pass
-        
-class SqlSetup:
-    def __init__(self, db):
-        self.db = db
-        self.conn = sqlite3.Connection
-        self.cursor = sqlite3.Cursor
     
+    def create_game(self, connection, gameId, playerId, numberOfAttempts):
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO Game (GameId, PlayerId, NumberOfAttempts) VALUES (?, ?, ?)', 
+                            (gameId, playerId, numberOfAttempts))
+        connection.commit()
+
     def createDatabaseFromFile(self):
         print(f'Working in: {os.getcwd()}')
         try:
@@ -51,3 +40,15 @@ class SqlSetup:
             print('Database created successfully!')
         except Exception as ex:
             print(f'Error while creating database: {ex}')
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass

24ff46e35454109482bb24283622afde670643d0
karimbousaid <63607864+karimbousaid@users.noreply.github.com>
2024-09-12 13:30:40 +0200

    create login system

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__, static_folder='static')

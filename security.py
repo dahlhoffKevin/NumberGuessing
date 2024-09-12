24dc4d197c06a6e6e79235de97f4b90c674a6ee0
CasualBlackMetalEnjoyer1994 <julian.seyfferth@students.tbs1.de>
2024-09-12 13:02:29 +0200

    Impl Security

--- security.py
+++ security.py
@@ -4,18 +4,18 @@ import hashlib
 class Security:
     def __init__(self, key=None):
         if key is None:
-            self.__key = self._load_key()
+            self.__key = self.__load_key()
         else:
             self.__key = key.encode()
         self.__cipher_suite = Fernet(self.__key)
 
-    def _write_key(self):
-        """Generiert einen Schluessel und speichert ihn in einer Datei."""
-        key = Fernet.generate_key()
-        with open("key.key", "wb") as key_file:
-            key_file.write(key)
+    # def _write_key(self):
+    #     """Generiert einen Schluessel und speichert ihn in einer Datei."""
+    #     key = Fernet.generate_key()
+    #     with open("key.key", "wb") as key_file:
+    #         key_file.write(key)
 
-    def _load_key(self):
+    def __load_key(self):
         """Laedt den Schluessel aus der Datei."""
         return open("key.key", "rb").read()
 
@@ -39,3 +39,19 @@ class Security:
         """Erstellt einen Hash-Wert aus einem String."""
         hash_object = hashlib.sha256(data.encode())
         return hash_object.hexdigest()
+    
+if __name__ == "__main__":
+    security = Security()
+    original_data = "Geheime Nachricht"
+    encrypted_data = security.encrypt(original_data)
+    print(f"Verschluesselte Daten: {encrypted_data}")
+
+    decrypted_data = security.decrypt(encrypted_data)
+    print(f"Entschluesselte Daten: {decrypted_data}")
+
+    print(f"Ist verschluesselt: {security.is_encrypted(encrypted_data)}")
+    print(f"Ist verschluesselt: {security.is_encrypted(original_data)}")
+
+    # Verwendung der statischen Methode BuildHash
+    hash_value = Security.BuildHash(original_data)
+    print(f"Hash-Wert: {hash_value}")    

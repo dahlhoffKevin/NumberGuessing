b1d6c7b51454c2456fdb97a0e6fad54d08aed5a7
karimbousaid <63607864+karimbousaid@users.noreply.github.com>
2024-09-19 14:28:05 +0200

    implement statment for the game

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sqlite3
 from flask import Flask, flash, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime, timedelta
 from settings.Settings import Settings
 from sql import Sql
 import os
 import uuid
 from random import randint
+
 
 sql = Sql.SQL(Settings.DatabasePath)
 # handling database setup (db creation, table creation)
@@ -18,7 +19,8 @@ conn = sql.open_conn()
 
 # game variables
 randomGameInt = randint(0, 101)
-numberOfAttempts = 0
+numberOfAttempts = 1
+result = ''
 
 # create flask app
 app = Flask(__name__, static_folder='static')
@@ -28,9 +30,8 @@ app.secret_key = 'LctYxG3mcQGhZ6DeVLdkxD0cEWe2Ye3axJ3vecg5emE='
 gameId = str(uuid.uuid4())
 
 @app.route('/')
-def hello_world():
-    print(f'gameId: {gameId}')
-    print(randomGameInt)
+def home():
+    print(randomGameInt);
     return render_template('login.html')
 
 @app.route('/submit', methods=['POST'])
@@ -38,25 +39,42 @@ def submit_attempt():
     global numberOfAttempts  # Declare as global
     global randomGameInt  # Declare as global to use in comparison
 
-    player_name = 'Testplayer' #fetch playername
+    player_id = session.get('playerId')
     number = request.form.get('number')
-    print(number)
-    # get player id from player_name?
-    temp_userID = str(uuid.uuid4())
 
-    numberOfAttempts += 1
+    if not number or not number.strip().isdigit():
+        flash('You must enter a valid number!', 'error')
+        return redirect(url_for('success'))
+    
+    number = int(number)
 
-    if int(number) == int(randomGameInt):
-        sql.create_game(conn, gameId, temp_userID, numberOfAttempts)
-        print("you have won")
+    if number > randomGameInt:
+        result = 'too large'
+        numberOfAttempts += 1
+        flash('Try Gessing again!', 'error')
+    elif number < randomGameInt:
+        result = 'too small'
+        numberOfAttempts += 1
+        flash('Try Gessing again!', 'error')
+    elif number == randomGameInt:
+        result = 'winning'
+        #Store data to game table
+        sql.create_game(conn, gameId, player_id, numberOfAttempts)
         return render_template('won.html')
+    # Store the result in the session
+    session['result'] = result
 
-    return redirect(url_for('hello_world'))
+    # Redirect to the success page
+    return redirect(url_for('success'))
+
 
 
 # Route for the registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    # Check if player is already logged in
+    if 'playername' in session:
+        return redirect(url_for('success'))
     if request.method == 'POST':
         playername = request.form['playername']
         mailaddress = request.form['mailaddress']
@@ -71,18 +89,22 @@ def register():
         conn.execute('INSERT INTO Player (PlayerId, Playername, MailAdress, Password) VALUES (?, ?, ?, ?)',
                      (player_id, playername, mailaddress, hashed_password))
         conn.commit()
+        session.permanent = True
 
         # Flash a success alert message
         flash('Welcome to the Guessing Number Game!', 'success')
         session['playername'] = playername;
         return redirect(url_for('success'))
-
     return render_template('register.html')
 
 
 # Route for the login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Check if player is already logged in
+    if 'playername' in session:
+        return redirect(url_for('success'))
+    
     if request.method == 'POST':
         mailaddress = request.form['mailaddress']
         password = request.form['password']
@@ -104,7 +126,7 @@ def login():
             # Check the password
             if check_password_hash(db_password, password):
                 flash(f'Welcome {player[1]}! You can play now.', 'success')
-                session['playername'] = player[0];
+                session['playerId'] = player[0];
                 session['playername'] = player[1];
                 return redirect(url_for('success'))
             else:
@@ -121,12 +143,14 @@ def login():
 
 @app.route('/success')
 def success():
-    #playername = session.get('playername')
+    playerId = session.get('PlayerId')
     playername = session.get('playername')
+    result = session.get('result', '')
     if not playername:
         return redirect(url_for('login'))
-    return render_template('index.html', playername=playername)
+    return render_template('index.html', playername=playername, playerId=playerId, result=result)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=30)
